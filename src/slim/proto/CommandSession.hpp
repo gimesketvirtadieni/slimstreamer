cbbb9a224c4ccba693ca70a8def84ae7c8c283c7
Andrej Kislovskij <gimesketvirtadieni@gmail.com>
2018-04-10 23:01:44 +0300

    Reverting back to sync send of SlimProto commands

--- src/slim/proto/CommandSession.hpp
+++ src/slim/proto/CommandSession.hpp
@@ -307,8 +307,7 @@ namespace slim
 				template<typename CommandType>
 				inline void send(CommandType command)
 				{
-					// TODO: introduce buffer wrapper so it can be passed to a stream; then in can be moved to a Command class
-					connection.writeAsync(command.getBuffer(), command.getSize());
+					connection.write(command.getBuffer(), command.getSize());
 				}
 
 			private:
