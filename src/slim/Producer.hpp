c3200e8e13299cae63fdc09cfac1b7eb85e7ae20
Andrej Kislovskij <gimesketvirtadieni@gmail.com>
2018-05-11 23:34:29 +0300

    Producer API refactoring

--- src/slim/Producer.hpp
+++ src/slim/Producer.hpp
@@ -26,7 +26,7 @@ namespace slim
 			virtual bool isAvailable() = 0;
 			virtual bool isRunning() = 0;
 			virtual void pause(unsigned int millisec) = 0;
-			virtual bool produce(std::reference_wrapper<Consumer>) = 0;
+			virtual bool produce(Consumer&) = 0;
 			virtual void start(std::function<void()> = [] {}) = 0;
 			virtual void stop(bool gracefully = true) = 0;
 	};
