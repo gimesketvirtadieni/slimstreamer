9ca0c44607e614e22c6866790de9725359adb2d5
Andrej Kislovskij <gimesketvirtadieni@gmail.com>
2019-08-11 23:30:50 +0300

    Migrated compilation to cmake

---
+++ cmake/FindFLAC.cmake
@@ -0,0 +1,7 @@
+
+# TODO: implement proper package resolution
+#find_library(FLAC_LIBRARY
+#    NAMES flac libflac
+#    HINTS ${_FLAC_LIBRARY_DIRS})
+
+#add_target_library(${LOCAL_PROJECT_NAME} UNKNOWN IMPORTED)
